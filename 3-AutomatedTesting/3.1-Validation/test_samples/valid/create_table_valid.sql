336026be4865000f88d4058f91671e65a220a4b8
Slava <slavasolomahin12@gmail.com>
2025-06-13 13:19:09 +0300

    sql fix

--- 3-AutomatedTesting/3.1-Validation/test_samples/valid/create_table_valid.sql
+++ 3-AutomatedTesting/3.1-Validation/test_samples/valid/create_table_valid.sql
@@ -1,8 +1,8 @@
 CREATE TABLE votes (
-  id INTEGER PRIMARY KEY,
-  election_id INTEGER NOT NULL,
-  voter_id INTEGER NOT NULL,
-  candidate_id INTEGER NOT NULL,
-  timestamp DATETIME NOT NULL,
-  hash TEXT NOT NULL
+    id INTEGER PRIMARY KEY,
+    election_id INTEGER NOT NULL,
+    voter_id INTEGER NOT NULL,
+    candidate_id INTEGER NOT NULL,
+    timestamp DATETIME NOT NULL,
+    hash TEXT NOT NULL
 );
